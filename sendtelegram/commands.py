253cd3bb7bc2ee416b634ea22fe3dde23754f0d3
gstracqu <giovanni.stracquadanio@ed.ac.uk>
2020-01-02 15:13:31 +0000

    beta release. prep pypi code

--- sendtelegram/commands.py
+++ sendtelegram/commands.py
@@ -1,29 +1,43 @@
 import os
 import sys
+import logging
 import requests
 import textwrap
 
+__SENDTELEGRAM_API_TOKEN__ = "SENDTELEGRAM_API_TOKEN"
+__SENDTELEGRAM_CHAT_ID__ = "SENDTELEGRAM_CHAT_ID"
 
-def send_message(msg_max_len: 'Max char per Telegram message' = 4096):
+
+def send_message(max_msg_len: "Max number of characters of a Telegram message" = 4096):
+    if (
+        os.getenv(__SENDTELEGRAM_API_TOKEN__) is None
+        or os.getenv(__SENDTELEGRAM_CHAT_ID__) is None
+    ):
+        logging.error(
+            "{} or {} are not set".format(
+                __SENDTELEGRAM_API_TOKEN__, __SENDTELEGRAM_CHAT_ID__
+            )
+        )
+        return -1
+
     # stubs for api url
-    telegram_api_url = "https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage".format(
-        TELEGRAM_API_TOKEN=os.getenv("TELEGRAM_API_TOKEN")
+    telegram_api_url = "https://api.telegram.org/bot{SENDTELEGRAM_API_TOKEN}/sendMessage".format(
+        SENDTELEGRAM_API_TOKEN=os.getenv(__SENDTELEGRAM_API_TOKEN__)
     )
+
     # stubs for api data
-    telegram_api_data = {"chat_id": os.getenv("TELEGRAM_CHAT_ID"), "text": ""}
+    telegram_api_data = {"chat_id": os.getenv(__SENDTELEGRAM_CHAT_ID__), "text": ""}
 
     # buffer all stdin messages
     payload = "".join([_ for _ in sys.stdin])
 
     # wrap text at telegram max length but keeping formatting
-    fmt_payload = textwrap.wrap(payload, width=256, replace_whitespace=False)
+    fmt_payload = textwrap.wrap(payload, width=max_msg_len, replace_whitespace=False)
 
     # send each formatted payload to
     for _ in fmt_payload:
-        telegram_api_data['text'] = _
-        r = requests.post(
-            telegram_api_url, data= telegram_api_data
-        )
+        telegram_api_data["text"] = _
+        r = requests.post(telegram_api_url, data=telegram_api_data)
 
         if r.status_code != requests.codes.ok:
             return -1
--- setup.py
+++ setup.py
@@ -4,40 +4,31 @@ from setuptools import find_packages, setup
 # determining the directory containing setup.py
 setup_path = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(setup_path, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(setup_path, "README.md"), encoding="utf-8") as f:
     readme = f.read()
 
 setup(
     # package information
-    name = 'sendtelegram',
-    packages = find_packages(),
-    version = '0.0.1-dev',
-    description = 'A telegram replacement for sendmail.',
-    long_description = readme,
-    license = 'MIT',
-    url='git@github.com:gstracquadanio/sendtelegram.git',
-    keywords='',
-
-    # author information
-    author = 'Giovanni Stracquadanio',
-    author_email = 'giovanni.stracquadanio@ed.ac.uk',
-
+    name="sendtelegram",
+    packages=find_packages(),
+    version="0.0.1-dev",
+    description="A telegram replacement for sendmail.",
+    long_description=readme,
+    license="MIT",
+    url="git@github.com:gstracquadanio/sendtelegram.git",
+    keywords="",
+    #  author information
+    author="Giovanni Stracquadanio",
+    author_email="giovanni.stracquadanio@ed.ac.uk",
     # installation info and requirements
-    install_requires=[],
+    install_requires=["requests>=2.22", "argh>=0.26"],
     setup_requires=[],
-
     # test info and requirements
-    test_suite='tests',
+    test_suite="tests",
     tests_require=[],
-
     # package deployment info
     include_package_data=True,
     zip_safe=False,
-
     # all tools have cli interface
-    entry_points={
-        'console_scripts': [
-            'sendtelegram=sendtelegram.cli:main',
-        ],
-    },
+    entry_points={"console_scripts": ["sendtelegram=sendtelegram.cli:main"]},
 )
